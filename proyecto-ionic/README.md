47ab5499c6777725b3827c65c8c249cbb3db68e1
SandovalAguilar <ismael.sandoval.aguilar@gmail.com>
2023-03-15 22:11:24 -0600

    Proyecto con Angular

---
+++ proyecto-ionic/README.md
@@ -0,0 +1,4 @@
+Practica 4 - Primer Proyecto con Angular
+
+# 2086239-AplicacionesMoviles-33
+Ian Joshua Montoya Torres - Repositorio
---
+++ proyecto-ionic/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { IonicModule } from '@ionic/angular';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule, IonicModule, IonicModule.forRoot()
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
