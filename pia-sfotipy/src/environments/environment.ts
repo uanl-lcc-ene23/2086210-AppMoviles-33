984f13f4884170dbfebd68607ec52b5278d7fef4
Sandoval <ismael.sandoval.aguilar@gmail.com>
2023-04-25 21:43:53 -0600

    Configuracion de BD

---
+++ pia-sfotipy/src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyCD-TabgWNWYGc-elphZ9UW-YzHrIoi8K0",
+    authDomain: "aplicaciones-moviles-9981a.firebaseapp.com",
+    projectId: "aplicaciones-moviles-9981a",
+    storageBucket: "aplicaciones-moviles-9981a.appspot.com",
+    messagingSenderId: "987333606759",
+    appId: "1:987333606759:web:ab87a702240499744503c2",
+  }
+};
